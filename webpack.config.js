6abfa61733b0afae1061deec03ac5cc5f7f072c7
ppyang010 <644724460@qq.com>
2017-03-06 17:35:45 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path =require("path");
+module.exports = {
+  entry: path.resolve(__dirname,"./src/js/main.js"),
+  output: {
+    filename: path.resolve(__dirname,"./dist/bulid.js"),
+  },
+  module: {
+    loaders: [
+      //.js 文件使用 babel-loader 来编译处理
+      //{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+    {   test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+            presets: [ 'es2015','react']
+        }
+    },
+    {test: /\.json$/, loader: "json-loader"}
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: []
+};
